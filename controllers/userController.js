8f84a79a3fcc2c5e577f08e0e4acca56a50183db
rubenmrios <rubenmrios@outlook.es>
2021-01-10 18:58:42 +0100

    add dotenv

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 let connection = require('../config/db.js');
-let fs = require('fs');
 
 let userController = {};
 
@@ -7,11 +6,23 @@ let userController = {};
 
 userController.listUsers = (req, res) => {
 
+    console.log (process.env.DB_HOST) 
     let sql = `SELECT * FROM users`;
+
     connection.query(sql, (err, results) => {
         if (err) throw err;
         res.json(  {results} )
     })
 }
 
+userController.getOneUser = (req, res) => {
+    let user_id = req.params.user_id;
+
+    let sql = `SELECT * FROM users WHERE user_id= ${user_id}`;
+
+    connection.query(sql, (err, result) => {
+        if (err) throw err;
+        res.json(  {result} )
+    })
+}
 module.exports = userController;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
 
-/* GET users listing. */
+//localhost:3000/users
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+//localhost:3000/users/listUsers
 router.get('/listUsers', userController.listUsers);
+
+//localhost:3000/users/1 || //localhost:3000/users/2 ||  ....
+router.get('/:user_id', userController.getOneUser);
 
 module.exports = router;
