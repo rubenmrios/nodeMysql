dcd7e25a38d4d4be07142ba54f3e086624acc229
rubenmrios <rubenmrios@outlook.es>
2021-01-10 18:40:10 +0100

    change async

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,26 +5,13 @@ let userController = {};
 
 
 
-// userController.listUsers = (req, res) => {
+userController.listUsers = (req, res) => {
 
-//     let sql = `SELECT * FROM users`;
-//     connection.query(sql, (err, results) => {
-//         if (err) throw err;
-//         res.json(  {results} )
-//     })
-// }
-userController.listUsers = async  (req, res) => {
     let sql = `SELECT * FROM users`;
-try{
-    await   connection.query(sql, (err, results) => {
-        // if (err) throw err;
+    connection.query(sql, (err, results) => {
+        if (err) throw err;
         res.json(  {results} )
     })
-}catch (err){
-    console.error('--------------------------------------------------------------------');
-    console.error(err);
+}
 
-}
-   
-}
 module.exports = userController;
